a408bbb7c9db3527c2b5906c6e64846a597541e9
Sergio Vieri <sergio@vieri.tech>
2018-01-27 17:38:06 +0800

    Added namespace std

--- solve_bf.h
+++ solve_bf.h
@@ -1,4 +1,9 @@
+#ifndef SOLVE_BF_H
+#define SOLVE_BF_H
 #include<bits/stdc++.h>
+using namespace std;
 
 void build_bf(vector<vector<bitset<100> > > slot);
 priority_queue<solution> optimal_solution(int size);
+
+#endif
